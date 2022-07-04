d1c96749eca3bbbcec8bd39a1710a36c92127dae
Neha Singh <nehasinghrangi@gmail.com>
2022-07-04 20:25:26 +0530

    linkedlist day1

---
+++ linkedlist/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        linkedlist list=new linkedlist();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+
+        list.display();
+
+    }
+    
+}
